a642bc941baafe09123d39937182b19ecf22ce62
João Victor <joao.dipaay@gmail.com>
2024-08-30 15:58:52 -0300

    initial config

---
+++ src/main/java/com/joao/chatws/ChatwsApplication.java
@@ -0,0 +1,13 @@
+package com.joao.chatws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatwsApplication.class, args);
+	}
+
+}
